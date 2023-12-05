eb05b966f02a81bc9b7e8d2c2be9e52e8dc27d36
Journey18 <nghia.nguyentrung18@gmail.com>
2023-12-06 00:42:51 +0700

    upload

--- shoe.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const shoe = () => {
-  return <div>shoe</div>;
-};
-
-export default shoe;
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ShoesStore from "./component/ShoesProduct/ShoesStore";
+
+function App() {
+  return (
+    <div className="App container ">
+      <ShoesStore></ShoesStore>
+    </div>
+  );
+}
+
+export default App;
